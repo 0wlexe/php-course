a6cec26053a862896fbf7579c2659e3d2a250363
Bea <30362709+j4nedoe@users.noreply.github.com>
2022-09-29 01:04:05 -0300

    Add files via upload

---
+++ cl0wnbaby-example/form.php
@@ -0,0 +1,29 @@
+<style type="text/css">
+	body{
+		background: #000;
+		color: #FFF;
+	}
+	a:link{
+		background-color: none;
+		color: red;
+		display: inline-block;	
+	}
+	a:hover, a:active {
+  		background-color: none;
+  		color: blue;
+	}
+
+</style>
+
+<script type="text/javascript">
+	/*window.alert('Are you sure?');*/
+
+</script>
+
+</head>
+
+<body>
+	<center><h1> Assign</h1>
+
+</body>
+</html>
